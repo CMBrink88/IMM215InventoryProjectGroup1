c1af1e0ca6ca6eac39decf0c95528f2985ad7703
Michael Douglas <michaelperezdouglas@gmail.com>
2020-02-26 17:55:36 -0500

    Auto stash before rebase of "origin/Douglas'"

---
+++ Assets/Scripts/YellowKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Apples;
+namespace Apples
+{
+    public class YellowKey : MasterManager
+    {
+        public void Start()
+        {
+            keyColor.material.color = Color.yellow; //Changes the key color to Red
+        }
+
+    }
+}
