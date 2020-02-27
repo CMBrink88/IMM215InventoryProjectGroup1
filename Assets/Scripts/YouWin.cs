f707ff03b335880eee681e33179806e3d76f7072
Owen Engle <owen.engle@chatham.edu>
2020-02-27 18:57:18 -0500

    Created "You Win" script
    created YouWin script that creates a small popup window with "you win" message and button to close window when player enters final area. created empty gameobject with collider set to trigger inside of win area to hold script

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    bool winMessage;
+    public Rect winPopup = new Rect(20, 20, 120, 50);
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            winMessage = true;
+        }
+    }
+    void OnGUI()
+    {
+        if (winMessage == true)
+        {
+            winPopup = GUI.Window(0, winPopup, DrawWindow, "You Win!");
+        }
+    }
+    void DrawWindow(int windowID)
+    {
+        if (GUI.Button(new Rect(10, 20, 100, 20), "OK"))
+        {
+            winMessage = false;
+        }
+    }
+}
