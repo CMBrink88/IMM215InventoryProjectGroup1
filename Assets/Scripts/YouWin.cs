561c7f5b468aee650c0b5d4c01b7922cbc819ba4
Owen Engle <owen.engle@chatham.edu>
2020-02-27 18:20:34 -0500

    Created win script
    Created script to display "you win" on reaching the last area, created empty gameobject with collider as trigger to hold script

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("You Win!");
+        }
+    }
+}
